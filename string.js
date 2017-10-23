02a1621c8d2cf0576f4aaf1954a55d7434b57aeb
chepkoy <allankiplangat22@gmail.com>
2017-10-23 12:22:33 -0700

    Added javascript strings file

---
+++ string.js
@@ -0,0 +1,8 @@
+var message = 'Hello';
+alert(message);
+message = "goodbye";
+alert(message);
+message = "She's a person";
+var HtmlSnippet = '<h1 class="Special> Important Headlines</h1>';
+
+message = "Shes\'s a great person"
